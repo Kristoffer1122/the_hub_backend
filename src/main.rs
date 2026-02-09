835e154304bc1fe6d1fad2268ea78772bc8dfee1
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-09 16:53:35 +0100

    use module from db file

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use dotenv::dotenv;
+use std::{
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    dotenv().ok();
+
+    let listener = TcpListener::bind("127.0.0.1:7879").unwrap();
+    println!("Listener: {:?}", listener);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+mod db;
+
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Incoming Stream {http_request:#?}");
+
+    let parts: Vec<&str> = http_request[0].split(" ").collect();
+    println!("Get Request: {}", parts[1]);
+
+    println!("Querying database");
+
+    db::api::query_db(parts[1]);
+}
