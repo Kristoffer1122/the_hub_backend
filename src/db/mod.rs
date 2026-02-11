460cd2434c6ad24cfd5b6745b63bb3232c57b37e
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-11 09:48:23 +0100

    serves data from db

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,21 +1,32 @@
 pub mod api {
-    use diesel::mysql::MysqlConnection;
+    use crate::schema::games;
+    use diesel::mysql::{Mysql, MysqlConnection};
     use diesel::prelude::*;
     use dotenv::dotenv;
 
+    #[derive(Debug, Queryable, Selectable)]
+    #[diesel(table_name = games)]
+    #[diesel(check_for_backend(Mysql))]
+    pub struct Game {
+        pub id: i32,
+        pub title: String,
+        pub genre: String,
+        pub utgivelsesdato: Option<chrono::NaiveDate>,
+    }
+
     pub fn connect_db() -> Result<MysqlConnection, Box<dyn std::error::Error>> {
+        println!("Checking database...");
         dotenv().ok();
 
-        let user = dotenv::var("DB_USER");
-        let password = dotenv::var("DB_PASSWORD");
-        let port = dotenv::var("DB_PORT");
-        let db_name = dotenv::var("DB_NAME");
+        let user = std::env::var("DB_USER")?;
+        let password = std::env::var("DB_PASSWORD")?;
+        let port = std::env::var("DB_PORT")?;
+        let db_name = std::env::var("DB_NAME")?;
 
         let url = format!(
             "mysql://{}:{}@localhost:{}/{}",
-            user?, password?, port?, db_name?,
+            &user, &password, &port, &db_name,
         );
-        println!("Database connection vars: {:#?}", url);
 
         let conn = MysqlConnection::establish(&url)
             .map_err(|e| format!("Failed to connect to the database: {}", e))?;
@@ -28,13 +39,17 @@ pub mod api {
         let user = dotenv::var("DB_USER");
         println!("db user: {:?}", user);
 
+        // save connection, but its waste
         match connect_db() {
-            Ok(_conn) => {
-                println!("Connected to the database successfully!");
-                // You can perform database operations here using `conn`
+            Ok(mut _conn) => {
+                println!("Querying database");
+                let results = games::table
+                    .select(Game::as_select())
+                    .load::<Game>(&mut _conn);
+                println!("Games: {:?}", results);
             }
             Err(e) => {
-                println!("Failed to connect to the database: {}", e);
+                println!("Error connecting to database: {}", e);
             }
         }
     }
