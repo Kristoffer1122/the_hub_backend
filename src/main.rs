330e7022f49dbe9e71b9b9a39e82b07e6aa601fc
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-11 11:08:14 +0100

    Add webframework, async functions

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,16 @@
-use dotenv::dotenv;
-use std::{
-    io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream},
-};
-
 mod db;
 mod schema;
 
-fn main() {
-    dotenv().ok();
+use axum::{Router, routing::get};
 
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    println!("Listener: {:?}", listener);
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/games", get(db::api::get_games));
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+    // can unwrap if port is unavaliable
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
 
-        handle_connection(stream);
-    }
-}
+    println!("Server running on: localhost:7878 ");
 
-fn handle_connection(stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("Incoming Stream {http_request:#?}");
-
-    let parts: Vec<&str> = http_request[0].split(" ").collect();
-    println!("Get Request: {}", parts[1]);
-
-    println!("Querying database");
-
-    db::api::query_db(parts[1]);
+    axum::serve(listener, app).await.unwrap();
 }
