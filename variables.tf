0f03e3ae3cbdd7007f85831749d2a4840a673c6c
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-11 13:34:57 +0100

    terraform variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "db_password" {
+  description = "MySQL password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_user" {
+  description = "MySQL user"
+  type        = string
+  default     = "user"
+}
+
+variable "db_name" {
+  description = "MySQL database name"
+  type        = string
+  default     = "gamelibrary"
+}
+
+variable "db_port" {
+  description = "MySQL database port"
+  type        = number
+  default     = 3306
+}
