386d995fd3b6ecef2a631cc77714a531b2269016
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-26 09:05:04 +0100

    routes for uploading and deletion

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod db;
 mod schema;
 
-use axum::{Router, routing::get};
+use axum::{Router, routing::get, routing::post};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/games", get(db::api::get_games));
+    let app = Router::new()
+        .route("/getgames", get(db::api::get_game))
+        .route("/games/:id", post(db::api::update_game))
+        .route("/creategames", post(db::api::create_game))
+        .route("/deletegames/:id", post(db::api::delete_game));
 
     // can unwrap if port is unavaliable
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
