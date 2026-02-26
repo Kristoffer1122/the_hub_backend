b770c2521d96d99309d9692157264c0608dc465e
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-26 09:15:36 +0100

    axiom routing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use axum::{Router, routing::get, routing::post};
 async fn main() {
     let app = Router::new()
         .route("/getgames", get(db::api::get_game))
-        .route("/games/:id", post(db::api::update_game))
+        .route("/games/{id}", post(db::api::update_game))
         .route("/creategames", post(db::api::create_game))
-        .route("/deletegames/:id", post(db::api::delete_game));
+        .route("/deletegames/{id}", post(db::api::delete_game));
 
     // can unwrap if port is unavaliable
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
