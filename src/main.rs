e5365af3e58318f409729180294eb9e0898ed3a2
Kristoffer1122 <krkja001@osloskolen.no>
2026-02-27 15:32:03 +0100

    better routes

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use axum::{Router, routing::get, routing::post};
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/getgames", get(db::api::get_game))
+        .route("/getgames", get(db::api::get_games))
         .route("/games/{id}", post(db::api::update_game))
-        .route("/creategames", post(db::api::create_game))
-        .route("/deletegames/{id}", post(db::api::delete_game));
+        .route("/creategame", post(db::api::create_game))
+        .route("/deletegame/{id}", post(db::api::delete_game));
 
     // can unwrap if port is unavaliable
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
