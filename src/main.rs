f5f58051ec96c123d06c0ae55e547a32b86c6f04
Kristoffer1122 <krkja001@osloskolen.no>
2026-03-02 15:22:32 +0100

    feat: add recap routes to server

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ async fn main() {
         .route("/getgames", get(db::api::get_games))
         .route("/games/{id}", post(db::api::update_game))
         .route("/creategame", post(db::api::create_game))
-        .route("/deletegame/{id}", post(db::api::delete_game));
+        .route("/deletegame/{id}", post(db::api::delete_game))
+        .route("/recap/{week}/{year}", get(db::api::get_weekly_recap))
+        .route("/saverecap", post(db::api::save_weekly_recap));
 
     // can unwrap if port is unavaliable
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7878").await.unwrap();
